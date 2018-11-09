e751822f2efc7d40963d1fad350db4cebec5d7be
freak07 <michalechner92@googlemail.com>
2018-11-09 20:07:10 +0100

    Revert [FIX] "zram: use __GFP_MOVABLE for memory allocation"
    This reverts commit 9bc482d3460501ac809457af26b46b72cd7dc212.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -669,9 +669,8 @@ compress_again:
 		handle = zs_malloc(meta->mem_pool, clen,
 				__GFP_KSWAPD_RECLAIM |
 				__GFP_NOWARN |
-				__GFP_HIGHMEM |
-				__GFP_MOVABLE |
-				__GFP_CMA);
+				__GFP_CMA) |
+				__GFP_HIGHMEM);
 	if (!handle) {
 		zcomp_stream_put(zram->comp);
 		zstrm = NULL;
@@ -680,7 +679,7 @@ compress_again:
 
 		handle = zs_malloc(meta->mem_pool, clen,
 				GFP_NOIO | __GFP_HIGHMEM |
-				__GFP_MOVABLE | __GFP_CMA);
+				__GFP_CMA);
 		if (handle)
 			goto compress_again;
 
