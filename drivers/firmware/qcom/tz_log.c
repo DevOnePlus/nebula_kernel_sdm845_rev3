d9b75e8f39e3453d64727e8fda2633d4974ad18e
Sultan Alsawaf <sultan@kerneltoast.com>
2018-11-25 18:20:39 -0800

    firmware: qcom: Remove debugfs dependency from tz-log driver
    Without this driver (specifically, its memory carveout), the device crashes
    frequently due to TZ log messages overwriting kernel memory, so this
    driver is needed even when debugfs is broken or missing.
    
    Change-Id: I3fda4c6d2e21673e62ccd6ee1acc37270de630fb
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/firmware/qcom/tz_log.c
+++ drivers/firmware/qcom/tz_log.c
@@ -1245,8 +1245,10 @@ static int tz_log_probe(struct platform_device *pdev)
 
 	tzdbg.diag_buf = (struct tzdbg_t *)ptr;
 
-	if (tzdbgfs_init(pdev))
-		goto err;
+	if (tzdbgfs_init(pdev)) {
+		kfree(tzdbg.diag_buf);
+		tzdbg.diag_buf = NULL;
+	}
 
 	if (tzprocfs_init(pdev))
 		goto err;
@@ -1256,9 +1258,6 @@ static int tz_log_probe(struct platform_device *pdev)
 	tzdbg_get_tz_version();
 
 	return 0;
-err:
-	kfree(tzdbg.diag_buf);
-	return -ENXIO;
 }
 
 
